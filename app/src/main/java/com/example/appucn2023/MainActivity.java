d096f2b28e24aa1fb2eb160d4947c622701aa46a
sebas <anderson.olarte@ias.com.co>
2023-11-02 20:43:41 -0500

    ss

--- app/src/main/java/com/example/appucn2023/MainActivity.java
+++ app/src/main/java/com/example/appucn2023/MainActivity.java
@@ -33,10 +33,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //se inicializan los editext y el boton con los recursos del xml
         txtnames = (EditText) findViewById(R.id.txtnames);
         txtpwd = (EditText) findViewById(R.id.txtpwd);
         btnsesion = (Button) findViewById(R.id.btnsesion);
 
+        //Si hay un evento en el boton de iniciar sesion, se llamara el metodo de iniciarSesion()
         btnsesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,25 +48,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void iniciarSesion() {
+        //Se crea la peticion
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         String url = "https://appucn2023.000webhostapp.com/login.php?"
                 .concat("user=").concat(txtnames.getText().toString())
                 .concat("&pwd=").concat(txtpwd.getText().toString());
+        //Se instancia la clase JsonObjectRequest para poder recibir el JSONObject del webservice
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                //Si los datos son correctos, ingresa al if
                 if (response.optString("mensaje").equals("OK")){
+                    //Se intancia la clase intent para redireccionar al MainActivity2 y adicional se envia el nombre por putExtra
                     Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
                     intent.putExtra("name", response.optString("name"));
                     startActivity(intent);
                 }else{
+                    //si los datos de los editext son incorrectos, se muestra el toast
                     Toast.makeText(getApplicationContext(), "Los datos ingresados son invalidos", Toast.LENGTH_LONG).show();
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
+                //si ocurre un error al consumir el webservice se muestra el toast
+                Toast.makeText(getApplicationContext(), "Error al consumir el servicio", Toast.LENGTH_LONG).show();
             }
         });
         requestQueue.add(jsonObjectRequest);
--- app/src/main/java/com/example/appucn2023/MainActivity2.java
+++ app/src/main/java/com/example/appucn2023/MainActivity2.java
@@ -14,9 +14,11 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+        //se inicializan los editext y el boton con los recursos del xml
         txtBienvenido = (TextView) findViewById(R.id.txtBienvenido);
-
+        //Se obtiene el intent de la clase MainActivity
         String nombre = getIntent().getStringExtra("name");
+        //Se asigna el nombre en el txtBienvenido
         txtBienvenido.setText("¡Bienvenido "+nombre+"!");
     }
 }
